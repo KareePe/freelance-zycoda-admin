1fcdb971398bbae0d531442cbab92968b396a6be
PePatPong <perapat.po@inet.co.th>
2024-05-25 13:14:36 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+    <Vueform>
+      <TextElement name="hello_world" label="Hello" placeholder="World" />
+    </Vueform>
+
+    <UButton label="Open" @click="isOpen = true" />
+
+    <UModal v-model="isOpen">
+      <UCard
+        :ui="{
+          ring: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+        }"
+      >
+        <template #header>
+          <Placeholder class="h-8" />
+        </template>
+
+        <Placeholder class="h-32" />
+
+        <template #footer>
+          <Placeholder class="h-8" />
+        </template>
+      </UCard>
+    </UModal>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = ref(false);
+</script>
